8e238b0758ff4d88ab45047851727dd352dd8054
davidmugambi104 <davidmugambi104@gmail.com>
2024-08-01 00:56:46 +0300

    adds Tag-module

--- backend/models.py
+++ backend/models.py
@@ -146,3 +146,17 @@ class Comment(db.Model):
             'created_at': self.created_at
 
         }
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Tag {self.name}>'
+    
+    def to_dict(self):
+                return {
+            'id': self.id,
+            'name':self.name
+
+        }
