2013b1df403611f40510d22a916b4dae906e5588
ElvisKiongo <elvis.kiongo@student.moringaschool.com>
2024-08-08 20:08:29 +0300

    Added Login form

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,14 +1,20 @@
+// src/App.js
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
 import Signup from './components/Signup';
-
+import Home from './components/Home';
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
-          <Route path="/home" component={Home} /> {}
+          <Route path="/home" component={Home} />
         </Switch>
       </div>
     </Router>
